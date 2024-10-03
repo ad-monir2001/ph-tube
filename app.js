7d0a37625dc50a3de261f131eb72660eee0db20f
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-10-03 14:53:01 +0600

    time formate done

--- app.js
+++ app.js
@@ -17,14 +17,39 @@ const categories = (data) => {
 fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
   .then((res) => res.json())
   .then((data) => videos(data));
+// time conversion
+const convertTime = (time) => {
+  console.log(time);
+  const timeNumber = parseInt(time);
+  const day = parseInt(timeNumber / 86400);
+  let remainingSecond = timeNumber % 86400;
+  const hour = parseInt(remainingSecond / 3600);
+  remainingSecond = remainingSecond % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  remainingSecond = remainingSecond % 60;
+  const second = remainingSecond;
+  console.log(second);
+  console.log();
+  return `${day} day ${hour} Hour ${minute} Minute ${second} second`;
+};
 
 const videos = (data) => {
   const videos = data.videos;
   for (let video of videos) {
+    console.log(video.others.posted_date);
     let div = document.createElement('div');
     div.classList.add('p-4', 'm-4', 'rounded-lg');
     div.innerHTML = `
-    <img src="${video.thumbnail}" class="rounded-lg w-80 h-52">
+    <div class="relative">
+      <img src="${video.thumbnail}" class="rounded-lg w-80 h-52">
+      ${
+        video.others.posted_date?.length == 0
+          ? ''
+          : `<span class="absolute bg-slate-700 text-white font-medium rounded-lg p-1 right-14 bottom-3 text-xs">${convertTime(
+              video.others.posted_date
+            )}</span> `
+      }
+    </div>
     <div class="flex gap-2 mt-4 items-start">
         <img src="${
           video?.authors[0]?.profile_picture
